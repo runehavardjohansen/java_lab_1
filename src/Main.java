e5429a91e4bb838ddcd739420dfa3e4c2af48261
runehavardjohansen <r.iwanicki@gmail.com>
2021-09-29 13:25:33 +0200

    Wykonano polecenie.

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,12 @@ public class Main {
 //        ćw.1 napisać po kilka przykładów wypisywania tekstu w konsoli,
 //        Użyć każdej z opcji
 
+System.out.println("To tekst pisany w jednej linii.");
+System.out.println("To jest tekst" + " + " + "dodatkowe wyrazy.");
+System.out.printf("To jest tekst, który zaciąga słowa, np. %s i %s oraz liczby %d bądź %d.", "woda","papier", 1488, 2137);
+System.out.println();
+System.out.print("To jest formuła *print*, która ");
+System.out.print("pozwala pisać ciągiem.");
 
     }
 }
